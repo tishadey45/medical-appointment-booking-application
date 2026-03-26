804ead674311caab37c3515e5f2aec8080a2e64c
Tisha Dey <tishadey2025@outlook.com>
2026-03-27 01:30:05 +0600

    :art: doctors card updated

--- src/components/BestDoctors.jsx
+++ src/components/BestDoctors.jsx
@@ -1,41 +1,98 @@
-import doctorImg from "../assets/doctor-sample.png";
+import doctorImg from "../assets/doctor-sample-2.png";
 
 const BestDoctors = () => {
   return (
-    <div>
-    <div className="bg-base-100 shadow-md rounded-2xl p-4 w-80 mx-auto">
-      <div className="rounded-xl overflow-hidden">
-        <img
-          src={doctorImg}
-          alt="doctor"
-          className="w-full h-52 object-cover"
-        />
-      </div>
-      <div className="flex gap-2 mt-3">
-        <span className="badge badge-success badge-soft rounded-full">
-          Available
-        </span>
-        <span className="badge badge-info badge-soft rounded-full">
-          5+ Years Experience
-        </span>
-      </div>
-      <div className="mt-3">
-        <h2 className="text-lg font-semibold">
-          Dr. Cameron Williamson
-        </h2>
-        <p className="text-gray-500 text-sm">
-          MBBS, MD - General Medicine, DNB
-        </p>
-      </div>
-      <div className="border-t my-3"></div>
-      <p className="text-sm text-gray-500 flex items-center gap-1">
-        ® Reg No: BD 12451254
-      </p>
-      <button className="btn btn-outline  w-full mt-4 rounded-full border border-info text-info">
-        View Details
-      </button>
-    </div>
+    <div className="max-w-7xl mx-auto px-16 py-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {/* Card 1 */}
+        <div className="bg-base-100 shadow-md rounded-2xl p-4">
+          <div className="rounded-xl overflow-hidden">
+            <img src={doctorImg} alt="doctor" className="w-full object-cover" />
+          </div>
 
+          <div className="flex gap-2 mt-3">
+            <span className="badge badge-success badge-soft rounded-full">
+              Available
+            </span>
+            <span className="badge badge-info badge-soft rounded-full">
+              5+ Years Experience
+            </span>
+          </div>
+
+          <div className="mt-3">
+            <h2 className="text-lg font-semibold">Dr. Cameron Williamson</h2>
+            <p className="text-gray-500 text-sm">
+              MBBS, MD - General Medicine, DNB
+            </p>
+          </div>
+
+          <div className="border-t my-3"></div>
+
+          <p className="text-sm text-gray-500">® Reg No: BD 12451254</p>
+
+          <button className="btn btn-outline w-full mt-4 rounded-full border border-info text-info">
+            View Details
+          </button>
+        </div>
+
+        {/* Card 2 */}
+        <div className="bg-base-100 shadow-md rounded-2xl p-4">
+          <div className="rounded-xl overflow-hidden">
+            <img src={doctorImg} alt="doctor" className="w-full object-cover" />
+          </div>
+
+          <div className="flex gap-2 mt-3">
+            <span className="badge badge-success badge-soft rounded-full">
+              Available
+            </span>
+            <span className="badge badge-info badge-soft rounded-full">
+              5+ Years Experience
+            </span>
+          </div>
+
+          <div className="mt-3">
+            <h2 className="text-lg font-semibold">Dr. Cameron Williamson</h2>
+            <p className="text-gray-500 text-sm">
+              MBBS, MD - General Medicine, DNB
+            </p>
+          </div>
+
+          <div className="border-t my-3"></div>
+          <p className="text-sm text-gray-500">® Reg No: BD 12451254</p>
+          <button className="btn btn-outline w-full mt-4 rounded-full border border-info text-info">
+            View Details
+          </button>
+        </div>
+
+        {/* Card 3 */}
+        <div className="bg-base-100 shadow-md rounded-2xl p-4">
+          <div className="rounded-xl overflow-hidden">
+            <img src={doctorImg} alt="doctor" className="w-full object-cover" />
+          </div>
+
+          <div className="flex gap-2 mt-3">
+            <span className="badge badge-success badge-soft rounded-full">
+              Available
+            </span>
+            <span className="badge badge-info badge-soft rounded-full">
+              5+ Years Experience
+            </span>
+          </div>
+
+          <div className="mt-3">
+            <h2 className="text-lg font-semibold">Dr. Cameron Williamson</h2>
+            <p className="text-gray-500 text-sm">
+              MBBS, MD - General Medicine, DNB
+            </p>
+          </div>
+
+          <div className="border-t my-3"></div>
+          <p className="text-sm text-gray-500">® Reg No: BD 12451254</p>
+          <button className="btn btn-outline w-full mt-4 rounded-full border border-info text-info">
+            View Details
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
